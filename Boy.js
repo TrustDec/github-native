97787485bd77d872223688d591b561d410c3f7c9
DESKTOP-PODTRFD\Trust <603049583@qq.com>
2017-04-03 01:52:29 +0800

    NavigationBar Component

--- Boy.js
+++ Boy.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactNative from 'react-native';
 const {View,Text,StyleSheet} = ReactNative;
 import Girl from './Girl';
+import NavigationBar from './NavigationBar';
 export default class Boy extends Component {
   constructor(props){
     super(props);
@@ -12,6 +13,15 @@ export default class Boy extends Component {
   render() {
   	return(
   		<View style={styles.container}>
+  			<NavigationBar 
+  				title={"Boy"}
+  				statusBar={{
+  					backgroundColor: '#ee6363'
+  				}}
+  				style={{
+		            backgroundColor: '#ee6363'
+		          }}
+  			/>
   			<Text style={styles.text}>I am boy</Text>
   			<Text style={styles.text}
   				onPress={()=>{
@@ -35,8 +45,7 @@ export default class Boy extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: 'gray',
-		justifyContent: 'center'
+		backgroundColor: '#fff',
 	},
 	text: {
 		fontSize: 20,
--- Girl.js
+++ Girl.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import ReactNative from 'react-native';
-const {View,Text,StyleSheet} = ReactNative;
+
+const {View,Text,Image,StyleSheet,TouchableOpacity} = ReactNative;
+
+import NavigationBar from './NavigationBar';
+
 export default class Girl extends Component {
   constructor(props){
     super(props);
@@ -8,9 +12,24 @@ export default class Girl extends Component {
       word: ''
     }
   }
+  renderButton(image) {
+    return <TouchableOpacity
+      onPress={()=>{
+        this.props.navigator.pop()
+      }}
+      >
+      <Image style={styles.back_pop} source={image}/>
+    </TouchableOpacity>
+  }
   render() {
   	return(
   		<View style={styles.container}>
+        <NavigationBar
+          title={ 'Girl' }
+          style={{ backgroundColor: '#ee6363' }}
+          leftButton={this.renderButton(require('./res/images/ic_arrow_back_white_36pt.png'))}
+          rightButton={this.renderButton(require('./res/images/ic_star.png'))}
+        />
         <Text style={styles.text}>I am Girl</Text>
         <Text style={styles.text}>我收到了男孩送的:{this.props.word}</Text>
         <Text style={styles.text}
@@ -28,10 +47,14 @@ export default class Girl extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: 'red',
-		justifyContent: 'center'
+    backgroundColor: '#fff',
 	},
 	text: {
-		fontSize: 20,
-	}
+		fontSize: 18,
+	},
+  back_pop: {
+    width: 22,
+    height: 22,
+    margin: 5
+  }
 });
